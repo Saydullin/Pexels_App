5ac8442ff60b870ca644dea17dfca4163e8c2f3e
Saydullin <saydullindev@gmail.com>
2023-11-07 16:33:29 +0300

    Colors added

---
+++ app/src/main/java/com/saydullin/pexelsapp/ui/colors/Colors.kt
@@ -0,0 +1,24 @@
+package com.saydullin.pexelsapp.ui.colors
+
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+
+private val LightThemeColors = lightColors(
+    primary = LightColors.Red,
+    secondary = LightColors.LightGray,
+    background = LightColors.White,
+    onSurface = LightColors.Black,
+    onPrimary = LightColors.White,
+    onSecondary = LightColors.Black,
+)
+
+private val DarkThemeColors = darkColors(
+    primary = DarkColors.Red,
+    secondary = DarkColors.DarkGray,
+    background = DarkColors.Black,
+    onSurface = DarkColors.LightGray,
+    onPrimary = DarkColors.White,
+    onSecondary = DarkColors.White,
+)
+
+
