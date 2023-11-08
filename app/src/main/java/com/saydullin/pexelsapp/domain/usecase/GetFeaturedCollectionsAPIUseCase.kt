342c34d545b1b4090be5605f660523af4780f53f
Saydullin <saydullindev@gmail.com>
2023-11-08 22:30:47 +0300

    GetFeaturedCollectionsAPIUseCase added

---
+++ app/src/main/java/com/saydullin/pexelsapp/domain/usecase/GetFeaturedCollectionsAPIUseCase.kt
@@ -0,0 +1,18 @@
+package com.saydullin.pexelsapp.domain.usecase
+
+import com.saydullin.pexelsapp.domain.model.CollectionFeatured
+import com.saydullin.pexelsapp.domain.repository.FeaturedCollectionsRepository
+import com.saydullin.pexelsapp.domain.utils.Resource
+import javax.inject.Inject
+
+class GetFeaturedCollectionsAPIUseCase @Inject constructor(
+    private val featuredCollectionsRepository: FeaturedCollectionsRepository,
+) {
+
+    suspend fun execute(): Resource<CollectionFeatured> {
+        return featuredCollectionsRepository.getFeaturedCollectionsAPI()
+    }
+
+}
+
+
