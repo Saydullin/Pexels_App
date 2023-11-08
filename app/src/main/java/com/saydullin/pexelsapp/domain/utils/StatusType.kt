d4e7cce6524fc49c158ddde07b9b5a71040adb29
Saydullin <saydullindev@gmail.com>
2023-11-08 22:31:29 +0300

    Resourse & StatusType classes added

---
+++ app/src/main/java/com/saydullin/pexelsapp/domain/utils/StatusType.kt
@@ -0,0 +1,12 @@
+package com.saydullin.pexelsapp.domain.utils
+
+enum class StatusType {
+    SUCCESS,
+    UNKNOWN_ERROR,
+    SERVER_ERROR,
+    CONNECTION_ERROR,
+    DATABASE_ERROR,
+    DATA_ERROR,
+}
+
+
