bf2ddd18f1cd0892dc51ad392c9759f2334ae90c
Saydullin <saydullindev@gmail.com>
2023-11-08 22:32:41 +0300

    MainActivity EntryPoint fixed

--- app/src/main/java/com/saydullin/pexelsapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/saydullin/pexelsapp/presentation/activity/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.saydullin.pexelsapp
+package com.saydullin.pexelsapp.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import com.saydullin.pexelsapp.screens.HomeScreen
-import com.saydullin.pexelsapp.ui.theme.PexelTheme
+import com.saydullin.pexelsapp.presentation.screens.HomeScreen
+import com.saydullin.pexelsapp.presentation.ui.theme.PexelTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
