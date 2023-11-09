841936cf98bcb45b0bb9301f334bc1bb32a58ec0
Saydullin <saydullindev@gmail.com>
2023-11-09 19:03:23 +0300

    FeaturedList added

---
+++ app/src/main/java/com/saydullin/pexelsapp/presentation/components/FeaturedList.kt
@@ -0,0 +1,69 @@
+package com.saydullin.pexelsapp.presentation.components
+
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.saydullin.pexelsapp.R
+import com.saydullin.pexelsapp.presentation.viewmodel.FeaturedCollectionsViewModel
+
+@Composable
+fun FeaturedList(
+    viewModel: FeaturedCollectionsViewModel = hiltViewModel()
+) {
+
+    val cornerRadius = dimensionResource(R.dimen.corner_radius)
+    val featuredList = viewModel.collectionFeatured.value ?: listOf()
+    val context = LocalContext.current
+
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(featuredList) { featured ->
+            val isActive = featured == featuredList.first()
+
+            Container(
+                isActive = isActive,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(cornerRadius))
+                    .clickable {
+                        Toast.makeText(context, featured.title, Toast.LENGTH_SHORT).show()
+                    },
+                innerPadding = PaddingValues(
+                    start = if (featured == featuredList.first())
+                        dimensionResource(R.dimen.side_margins) else 0.dp,
+                    end = if (featured == featuredList.last())
+                        dimensionResource(R.dimen.side_margins) else 0.dp
+                ),
+            ) {
+                Text(
+                    text = featured.title,
+                    style = if (isActive)
+                        typography.titleMedium
+                    else
+                        typography.labelMedium,
+                    color = if (isActive)
+                        colorScheme.onPrimary
+                    else
+                        colorScheme.onSecondary,
+                )
+            }
+        }
+    }
+
+}
+
+
