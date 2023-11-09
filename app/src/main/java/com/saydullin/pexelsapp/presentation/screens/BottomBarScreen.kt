e800491655a2c716aed5acdf12b48b21e58d6f83
Saydullin <saydullindev@gmail.com>
2023-11-10 01:31:09 +0300

    NavController & NavScreens added

---
+++ app/src/main/java/com/saydullin/pexelsapp/presentation/screens/BottomBarScreen.kt
@@ -0,0 +1,25 @@
+package com.saydullin.pexelsapp.presentation.screens
+
+import com.saydullin.pexelsapp.R
+
+sealed class Screen(
+    val route: String,
+    val title: String,
+    val icon: Int,
+    val activeIcon: Int
+) {
+    object Home : Screen(
+        route = "home",
+        title = "Home",
+        icon = R.drawable.home_icon,
+        activeIcon = R.drawable.home_icon_active
+    )
+    object Bookmark : Screen(
+        route = "bookmark",
+        title = "Bookmark",
+        icon = R.drawable.bookmark_icon,
+        activeIcon = R.drawable.bookmark_icon_active
+    )
+}
+
+
---
+++ app/src/main/java/com/saydullin/pexelsapp/presentation/screens/NavController.kt
@@ -0,0 +1,79 @@
+package com.saydullin.pexelsapp.presentation.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@SuppressLint("UseCompatLoadingForDrawables")
+@Composable
+fun NavController() {
+
+    val navController = rememberNavController()
+    val items = listOf(
+        Screen.Home,
+        Screen.Bookmark
+    )
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+
+                items.forEach { screen ->
+                    val isSelected = currentDestination?.route == screen.route
+                    BottomNavigationItem(
+                        modifier = Modifier
+                            .background(Color.White),
+                        icon = {
+                            Icon(
+                                painter = if (isSelected) {
+                                    painterResource(screen.activeIcon)
+                                } else {
+                                    painterResource(screen.icon)
+                                },
+                                contentDescription = screen.title,
+                            )
+                        },
+                        selected = isSelected,
+                        onClick = {
+                            navController.navigate(screen.route)
+                        }
+                    )
+                }
+            }
+        }
+    ) { padding ->
+        Spacer(modifier = Modifier
+            .padding(padding)
+        )
+        NavHost(
+            navController = navController,
+            startDestination = "home"
+        ) {
+            composable("home") {
+                HomeScreen()
+            }
+            composable("bookmark") {
+                BookmarkScreen()
+            }
+        }
+    }
+
+}
+
+
