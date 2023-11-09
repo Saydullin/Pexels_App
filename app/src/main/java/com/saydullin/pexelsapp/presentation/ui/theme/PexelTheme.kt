a8973c6b0c5f76caf277b9e4ef56a55e24e3f84e
Saydullin <saydullindev@gmail.com>
2023-11-09 19:21:54 +0300

    PexelTheme statusBar color set

--- app/src/main/java/com/saydullin/pexelsapp/presentation/ui/theme/PexelTheme.kt
+++ app/src/main/java/com/saydullin/pexelsapp/presentation/ui/theme/PexelTheme.kt
@@ -1,12 +1,17 @@
 package com.saydullin.pexelsapp.presentation.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import com.saydullin.pexelsapp.presentation.ui.colors.DarkThemeColors
 import com.saydullin.pexelsapp.presentation.ui.colors.LightThemeColors
 import com.saydullin.pexelsapp.presentation.ui.typegrophy.PexelTypography
@@ -14,14 +19,23 @@ import com.saydullin.pexelsapp.presentation.ui.typegrophy.PexelTypography
 @Composable
 fun PexelTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit,
+    content: @Composable () -> Unit,
 ) {
+
     val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
     val colors = when {
         dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
         dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
         darkTheme -> DarkThemeColors
         else -> LightThemeColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.background.toArgb() // change color status bar here
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
