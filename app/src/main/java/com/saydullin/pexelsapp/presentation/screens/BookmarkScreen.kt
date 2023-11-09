2b75b93f9092bb2ef20171d6ce1f82cfbe022641
Saydullin <saydullindev@gmail.com>
2023-11-10 01:30:00 +0300

    HomeScreen & BookmarkScreen added

---
+++ app/src/main/java/com/saydullin/pexelsapp/presentation/screens/BookmarkScreen.kt
@@ -0,0 +1,16 @@
+package com.saydullin.pexelsapp.presentation.screens
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BookmarkScreen() {
+
+    Surface {
+        Text(text = "Bookmark Screen")
+    }
+
+}
+
+
--- app/src/main/java/com/saydullin/pexelsapp/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/saydullin/pexelsapp/presentation/screens/HomeScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.saydullin.pexelsapp.R
@@ -14,10 +13,6 @@ import com.saydullin.pexelsapp.presentation.viewmodel.FeaturedCollectionsViewMod
 
 @Composable
 fun HomeScreen(viewModel: FeaturedCollectionsViewModel = hiltViewModel()) {
-
-    val collections = viewModel.collectionFeatured.value
-    val statusType = viewModel.statusType.value
-    val context = LocalContext.current
 
     LaunchedEffect(Unit) {
         viewModel.execute()
